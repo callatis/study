1ce0fa08f398377f1331e9867cfa6192e92b2d82
Mihai Poplacenel <mpoplacenel@ebay.com>
2016-07-30 16:23:46 -0700

    Added lowest bit position and bit count

--- src/main/java/org/callatis/study/utils/BitFun.java
+++ src/main/java/org/callatis/study/utils/BitFun.java
@@ -4,8 +4,30 @@ public class BitFun {
 	
 	private BitFun() {}
 	
-	public static int lowestSetBit(int a) {
+	public static int lowestSetBitValue(int a) {
 		return a & ~(a - 1);
+	}
+	
+	public static int lowestSetBitPosition(int a) {
+		for (int i = 0; i < 64; i++) {
+			int bit = 1 << i;
+			if ((bit & a) > 0) {
+				return i;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public static int countBits(int a) {
+		int aa = a, count = 0;
+		do {
+			int firstBitVal = BitFun.lowestSetBitValue(aa);
+			aa = aa & (~firstBitVal);
+			count++;
+		} while (aa > 0);
+		
+		return count;
 	}
 
 }
--- src/test/java/org/study/callatis/utils/BitFunTest.java
+++ src/test/java/org/study/callatis/utils/BitFunTest.java
@@ -9,26 +9,62 @@ public class BitFunTest {
 
 	@Test
 	public void testLowestSetBit1() {
-		assertEquals(1, BitFun.lowestSetBit(1));
-		assertEquals(1, BitFun.lowestSetBit(3));
-		assertEquals(1, BitFun.lowestSetBit(5));
-		assertEquals(1, BitFun.lowestSetBit(7));
+		assertEquals(1, BitFun.lowestSetBitValue(1));
+		assertEquals(1, BitFun.countBits(1));
+		assertEquals(1, BitFun.lowestSetBitValue(3));
+		assertEquals(2, BitFun.countBits(3));
+		assertEquals(1, BitFun.lowestSetBitValue(5));
+		assertEquals(2, BitFun.countBits(5));
+		assertEquals(1, BitFun.lowestSetBitValue(7));
+		assertEquals(3, BitFun.countBits(7));
 	}
 
 	@Test
 	public void testLowestSetBit2() {
-		assertEquals(2, BitFun.lowestSetBit(2));
-		assertEquals(2, BitFun.lowestSetBit(6));
-		assertEquals(2, BitFun.lowestSetBit(10));
-		assertEquals(2, BitFun.lowestSetBit(14));
+		assertEquals(2, BitFun.lowestSetBitValue(2));
+		assertEquals(1, BitFun.countBits(2));
+		assertEquals(2, BitFun.lowestSetBitValue(6));
+		assertEquals(2, BitFun.countBits(6));
+		assertEquals(2, BitFun.lowestSetBitValue(10));
+		assertEquals(2, BitFun.countBits(10));
+		assertEquals(2, BitFun.lowestSetBitValue(14));
+		assertEquals(3, BitFun.countBits(14));
 	}
 
 	@Test
 	public void testLowestSetBit3() {
-		assertEquals(4, BitFun.lowestSetBit(4));
-		assertEquals(4, BitFun.lowestSetBit(12));
-		assertEquals(4, BitFun.lowestSetBit(20));
-		assertEquals(4, BitFun.lowestSetBit(28));
+		assertEquals(4, BitFun.lowestSetBitValue(4));
+		assertEquals(1, BitFun.countBits(4));
+		assertEquals(4, BitFun.lowestSetBitValue(12));
+		assertEquals(2, BitFun.countBits(12));
+		assertEquals(4, BitFun.lowestSetBitValue(20));
+		assertEquals(2, BitFun.countBits(20));
+		assertEquals(4, BitFun.lowestSetBitValue(28));
+		assertEquals(3, BitFun.countBits(28));
+	}
+	
+	@Test
+	public void testLowestSetBitPosition1() {
+		assertEquals(0, BitFun.lowestSetBitPosition(1));
+		assertEquals(0, BitFun.lowestSetBitPosition(3));
+		assertEquals(0, BitFun.lowestSetBitPosition(5));
+		assertEquals(0, BitFun.lowestSetBitPosition(7));
 	}
 
+	@Test
+	public void testLowestSetBitPosition2() {
+		assertEquals(1, BitFun.lowestSetBitPosition(2));
+		assertEquals(1, BitFun.lowestSetBitPosition(6));
+		assertEquals(1, BitFun.lowestSetBitPosition(10));
+		assertEquals(1, BitFun.lowestSetBitPosition(14));
+	}
+
+	@Test
+	public void testLowestSetBitPosition3() {
+		assertEquals(2, BitFun.lowestSetBitPosition(4));
+		assertEquals(2, BitFun.lowestSetBitPosition(12));
+		assertEquals(2, BitFun.lowestSetBitPosition(20));
+		assertEquals(2, BitFun.lowestSetBitPosition(28));
+	}
+	
 }
