2f8bac28fe75966246e2d1802f58e0532231f482
Mihai Poplacenel <m_poplacenel@yahoo.com>
2026-06-18 22:59:28 -0700

    Small optimization

--- src/main/java/org/callatis/study/solutions/ThreeSumClosest.java
+++ src/main/java/org/callatis/study/solutions/ThreeSumClosest.java
@@ -3,8 +3,8 @@ package org.callatis.study.solutions;
 public class ThreeSumClosest {
     public int threeSumClosest(int[] nums, int target) {
         int minSum = target-Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     // System.out.println("Evaluating "+ i + ", " + j + ", " + k);
                     int newSum = nums[i] + nums[j] + nums[k];
