4f9d271e018ac1b3f8e3533c3a0e2222f310e3dc
Mihai Poplacenel <m_poplacenel@yahoo.com>
2026-06-28 15:20:55 -0700

    Renamed the Map var to avoid confusion

--- src/main/java/org/callatis/study/strings/LongestNoDupeSubstring.java
+++ src/main/java/org/callatis/study/strings/LongestNoDupeSubstring.java
@@ -8,21 +8,21 @@ public class LongestNoDupeSubstring {
     public int lengthOfLongestSubstring(String s) { // s = 
         if (s.length() <= 1) return s.length();
         int i = 0, len = s.length(), result = 0;
-        Map<Character, Integer> set = new HashMap<>();
+        Map<Character, Integer> map = new HashMap<>();
         while (i < len) { // i = 0
             for (int j = i; j < len; j++) { // j = 
                 char c = s.charAt(j); // c = 
-                if (set.containsKey(c)) { // ??
+                if (map.containsKey(c)) { // ??
                     // stop 
                     // check if we found a larger substring
                     result = Math.max(result, j - i); // result = 
                     // start next iter from the previous index where the j char appeared
-                    i = set.get(c) + 1; // ?? + 1 = ??
+                    i = map.get(c) + 1; // ?? + 1 = ??
                     // we resume from right after the previous position of the j char
-                    set.clear(); // set: [?? = ??, ...]
+                    map.clear(); // set: [?? = ??, ...]
                     break;
                 }
-                set.put(c, j); // set: [?? = ??, ...]
+                map.put(c, j); // set: [?? = ??, ...]
                 // if we reached the end of the string
                 if (j == len - 1) {
                     result = Math.max(result, j - i + 1);
