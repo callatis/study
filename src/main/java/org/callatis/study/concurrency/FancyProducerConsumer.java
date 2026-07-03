8480b4955a4caa0b62923a491cd7d14490b12221
Mihai Poplacenel <m_poplacenel@yahoo.com>
2026-07-02 21:19:47 -0700

    Added the ReentrantLock + Condition Producer Consumer implementation

---
+++ src/main/java/org/callatis/study/concurrency/FancyProducerConsumer.java
@@ -0,0 +1,75 @@
+package org.callatis.study.concurrency;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FancyProducerConsumer<T> implements ProducerConsumer<T> {
+
+    private final int capacity;
+
+    private final Queue<T> q;
+
+    private int n;
+
+    private final Lock lock = new ReentrantLock();
+    private final Condition notFull = lock.newCondition();
+    private final Condition notEmpty = lock.newCondition();
+
+    public FancyProducerConsumer(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity must be greater than zero");
+        }
+        this.capacity = capacity;
+        this.q = new LinkedList<>();
+    }
+
+    @Override
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    @Override
+    public int getQSize() {
+        lock.lock();
+        try {
+            return this.n;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void add(T item) throws InterruptedException {
+        lock.lock();
+        try {
+            while (n >= this.capacity) {
+                notFull.await();
+            }
+            this.n++;
+            q.add(item);
+            notEmpty.signalAll();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public T remove() throws InterruptedException {
+        T removed;
+        lock.lock();
+        try {
+            while (n <= 0) {
+                notEmpty.await();
+            }
+            this.n--;
+            removed = q.remove();
+            notFull.signalAll();
+        } finally {
+            lock.unlock();
+        }
+        return removed;
+    }
+}
--- src/test/java/org/callatis/study/concurrency/ProducerConsumerTest.java
+++ src/test/java/org/callatis/study/concurrency/ProducerConsumerTest.java
@@ -52,17 +52,21 @@ public class ProducerConsumerTest {
             {"BasicProducerConsumer", "test1x10x1000v1x10x1000", 1, 10, 1000, 1, 10, 1000},
             {"BasicProducerConsumer", "test2x10x1000v1x20x0", 2, 10, 1000, 1, 20, 0},
             {"BasicProducerConsumer", "test1x20x0v2x10x1000", 1, 20, 0, 2, 10, 1000},
-            {"BasicProducerConsumer", "test4x5x1000v2x10x500", 4, 5, 1000, 2, 10, 500},
-            {"BasicProducerConsumer", "test3x20x0v6x10x0", 3, 20, 0, 6, 10, 0},
-            {"BasicProducerConsumer", "test6x10x0v3x20x2000", 6, 10, 0, 3, 20, 2000},
-            {"BasicProducerConsumer", "test3x20x2000v6x10x0", 3, 20, 2000, 6, 10, 0},
-            {"BasicProducerConsumer", "test8x25x100v5x40x100", 8, 25, 100, 5, 40, 100},
-            {"BasicProducerConsumer", "test10x10x0v10x10x0", 10, 10, 0, 10, 10, 0},
-            {"BasicProducerConsumer", "test2x100x0v1x200x50", 2, 100, 0, 1, 200, 50},
-            {"BasicProducerConsumer", "test1x200x50v4x50x0", 1, 200, 50, 4, 50, 0},
-            {"BasicProducerConsumer", "test5x40x250v4x50x750", 5, 40, 250, 4, 50, 750},
-            {"BasicProducerConsumer", "test12x5x0v3x20x1500", 12, 5, 0, 3, 20, 1500},
-            {"BasicProducerConsumer", "test3x20x1500v12x5x0", 3, 20, 1500, 12, 5, 0},
+            {"FancyProducerConsumer", "test1x10x0v1x10x0", 1, 10, 0, 1, 10, 0},
+            {"FancyProducerConsumer", "test1x10x1000v1x10x1000", 1, 10, 1000, 1, 10, 1000},
+            {"FancyProducerConsumer", "test2x10x1000v1x20x0", 2, 10, 1000, 1, 20, 0},
+            {"FancyProducerConsumer", "test1x20x0v2x10x1000", 1, 20, 0, 2, 10, 1000},
+            // {"BasicProducerConsumer", "test4x5x1000v2x10x500", 4, 5, 1000, 2, 10, 500},
+            // {"BasicProducerConsumer", "test3x20x0v6x10x0", 3, 20, 0, 6, 10, 0},
+            // {"BasicProducerConsumer", "test6x10x0v3x20x2000", 6, 10, 0, 3, 20, 2000},
+            // {"BasicProducerConsumer", "test3x20x2000v6x10x0", 3, 20, 2000, 6, 10, 0},
+            // {"BasicProducerConsumer", "test8x25x100v5x40x100", 8, 25, 100, 5, 40, 100},
+            // {"BasicProducerConsumer", "test10x10x0v10x10x0", 10, 10, 0, 10, 10, 0},
+            // {"BasicProducerConsumer", "test2x100x0v1x200x50", 2, 100, 0, 1, 200, 50},
+            // {"BasicProducerConsumer", "test1x200x50v4x50x0", 1, 200, 50, 4, 50, 0},
+            // {"BasicProducerConsumer", "test5x40x250v4x50x750", 5, 40, 250, 4, 50, 750},
+            // {"BasicProducerConsumer", "test12x5x0v3x20x1500", 12, 5, 0, 3, 20, 1500},
+            // {"BasicProducerConsumer", "test3x20x1500v12x5x0", 3, 20, 1500, 12, 5, 0},
             {"BasicProducerConsumer", "test16x50x10v20x40x10", 16, 50, 10, 20, 40, 10}
         });
     }
@@ -124,6 +128,8 @@ public class ProducerConsumerTest {
     private <T> ProducerConsumer<T> createProducerConsumer(int capacity) {
         if ("BasicProducerConsumer".equals(implementationType)) {
             return new BasicProducerConsumer<>(capacity);
+        } else if ("FancyProducerConsumer".equals(implementationType)) {
+            return new FancyProducerConsumer<>(capacity);
         }
 
         throw new IllegalArgumentException("Unsupported implementation type: " + implementationType);
