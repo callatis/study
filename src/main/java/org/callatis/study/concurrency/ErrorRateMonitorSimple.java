64ea792d93a9d8cb3d2bdb9ae1c6a978e58a1e1d
Mihai Poplacenel <m_poplacenel@yahoo.com>
2026-07-20 18:22:40 -0700

    Improvements - from cosmetic to a single CAS

--- src/main/java/org/callatis/study/concurrency/ErrorRateMonitorSimple.java
+++ src/main/java/org/callatis/study/concurrency/ErrorRateMonitorSimple.java
@@ -5,11 +5,13 @@ import java.util.concurrent.atomic.AtomicLong;
 public class ErrorRateMonitorSimple {
 
     public static final int NUM_EXTRA_BUCKETS = 2; // current
+    private static final long RIGHT_INT_BITMASK = (1L << 32) - 1;
 
     private final int numBuckets;
     private final AtomicLong[] errors;
     private final AtomicLong[] successes;
     private final long nanoStart;
+
 
     public ErrorRateMonitorSimple(int numBuckets) {
         this.numBuckets = numBuckets + NUM_EXTRA_BUCKETS;
@@ -17,14 +19,14 @@ public class ErrorRateMonitorSimple {
         this.successes = new AtomicLong[this.numBuckets];
         this.nanoStart = System.nanoTime();
         // L = 5, currSec = 20:36:22
-        long currTimeSec = (getCurrentTimeInSeconds());
+        long currTimeSec = getCurrentTimeInSeconds();
         for (int i = 0; i < this.numBuckets; i++) {
             // inx = 2
             int inx = (int) (currTimeSec + i) % this.numBuckets;
-            long generation = currTimeSec + i;
-            this.errors[inx] = new AtomicLong(generation << 32); // right 32 bits, representing # errors, are 0
+            long bucketTimeSec = currTimeSec + i;
+            this.errors[inx] = new AtomicLong(bucketTimeSec << 32); // right 32 bits, representing # errors, are 0
             // 0 = 20:36:25, 1 = 20:36:26, 2 = 20:36:22, 3 = 20:36:23, 4 = 20:36:24
-            this.successes[inx] = new AtomicLong(generation << 32);
+            this.successes[inx] = new AtomicLong(bucketTimeSec << 32);
         }
     }
 
@@ -42,18 +44,19 @@ public class ErrorRateMonitorSimple {
 
     private void recordEvent(AtomicLong[] events, long currTimeSec) {
         int inx = (int) currTimeSec % this.numBuckets;
-        boolean resetNeeded = true;
-        while (resetNeeded) {
-            long bucketVal = events[inx].get();
-            // long rightQuadFull = (1L << 32) - 1;
-            long bucketTimeSec = bucketVal >> 32;
-            resetNeeded = (bucketTimeSec < currTimeSec);
-            if (resetNeeded) {
-                // only set it if nobody else incremented it in the meantime
-                resetNeeded = !events[inx].compareAndSet(bucketVal, (currTimeSec << 32));
+        while (true) {
+            long currSnapshot = events[inx].get();
+            long bucketTimeSec = currSnapshot >> 32;
+            long newSnapshot = currSnapshot;
+            if (bucketTimeSec < currTimeSec) { // we're in an old bucket - attempt to set a new bucket with a counter = 1
+                newSnapshot = (currTimeSec << 32) | 1L;
+            } else { // we're in the right bucket - just increment the counter, which is the same as incrementing the snapshot
+                newSnapshot++;
+            }
+            if (events[inx].compareAndSet(currSnapshot, newSnapshot)) {
+                return;
             }
         }
-        events[inx].incrementAndGet();
     }
 
     /**
@@ -66,23 +69,25 @@ public class ErrorRateMonitorSimple {
             timeWindowSec = this.numBuckets - NUM_EXTRA_BUCKETS - 1;
         }
 
-        long currTimeSec = getCurrentTimeInSeconds();
-        int i = (int) currTimeSec % this.numBuckets;
-        long rightQuadFull = (1L << 32) - 1;
+        final long currTimeSec = getCurrentTimeInSeconds();
+        int bucket = (int) currTimeSec % this.numBuckets;
         long numErrors = 0, numSuccesses = 0;
         for (int j = 0; j < timeWindowSec; j++) {
-            if (i == 0) i = this.numBuckets;
-            i--;
-            long e = this.errors[i].get();
-            if ((e >> 32) >= currTimeSec - this.numBuckets) {
-                numErrors += e & rightQuadFull;
-            }
-            long s = this.successes[i].get();
-            if ((s >> 32) >= currTimeSec - this.numBuckets) {
-                numSuccesses += s & rightQuadFull;
-            }
+            if (bucket == 0) bucket = this.numBuckets;
+            bucket--;
+            numErrors += addNumEvents(this.errors, bucket, currTimeSec);
+            numSuccesses += addNumEvents(this.successes, bucket, currTimeSec);
         }
         return (numErrors == 0 && numSuccesses == 0) ? 0.0 : (double) numErrors / (numErrors + numSuccesses);
+    }
+
+    private long addNumEvents(AtomicLong[] events, int bucket, final long currTimeSec) {
+        long snapshot = events[bucket].get();
+        if ((snapshot >> 32) >= currTimeSec - this.numBuckets) { // active bucket
+            return snapshot & RIGHT_INT_BITMASK; // extract the counter as the right int
+        }
+        // obsolete bucket, ignore
+        return 0;
     }
 
     private long getCurrentTimeInSeconds() {
